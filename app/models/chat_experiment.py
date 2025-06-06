27c29bfa5cfb218e7bb52f803ca5702da7b534b8
tung <tung.nx20042024@gmail.com>
2025-06-06 21:01:56 +0700

    update: trace logging chatbot Ha Giang

--- app/models/chat_experiment.py
+++ app/models/chat_experiment.py
@@ -58,6 +58,12 @@ from langchain.chat_models import init_chat_model
 
 model = init_chat_model("gemini-2.0-flash", model_provider="google_genai")
 
+model.invoke("Hello")
+
+model.invoke([{"role": "user", "content": "Hello"}])
+
+model.invoke([HumanMessage("Hello")])
+
 from langchain_core.messages import HumanMessage, SystemMessage
 
 messages = [
