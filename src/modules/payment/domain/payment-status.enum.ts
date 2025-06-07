b01cc16aa4a2c5cf2c90e5ec589e77789d1188fe
huy <huy.tg207988@sis.hust.edu.vn>
2025-06-07 20:45:02 +0700

    update payment status

---
+++ src/modules/payment/domain/payment-status.enum.ts
@@ -0,0 +1,10 @@
+// payment/domain/payment-status.enum.ts
+
+export enum PaymentStatus {
+  PENDING = 'PENDING',         // Chưa thanh toán
+  SUCCESS = 'SUCCESS',         // Thanh toán thành công
+  FAILED = 'FAILED',           // Thanh toán thất bại
+  CANCELED = 'CANCELED',       // Bị hủy bởi người dùng
+  EXPIRED = 'EXPIRED',         // Hết hạn thanh toán
+  REFUNDED = 'REFUNDED',       // Đã hoàn tiền
+}
